b3817738dd1e709c25716b148158f681c093161f
badalmainali <badalma12@gmail.com>
2022-08-09 00:54:38 +0545

    login

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+
+// register user
+router.post('/register',async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    } 
+})
+
+//login user
+router.post('/login',async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+})
