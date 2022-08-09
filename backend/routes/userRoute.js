9077c843040125a8321140607f02eae20276abd0
badalmainali <badalma12@gmail.com>
2022-08-09 23:12:28 +0545

    api error

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,20 +1,44 @@
 const express = require('express')
 const router = express.Router()
+const bcrypt = require('bcryptjs'); //for password hashing
+//importing the models
+const User = require("../models/userModels")
 
 // register user
-router.post('/register',async(req,res)=>{
+router.post('/register', async (req, res) => {
     try {
-        
+
+        //checking if user already exist
+        const userExist = await User.findOne({ 'email': req.body.email })
+        if (userExist) {
+            return res.status(400).send({ message: 'User already exists', success: false })
+
+        }
+        else {
+            const password = req.body.password
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(password, salt);
+            req.body.password = hashedPassword;
+            const newUser = new User(req.body); //get all values from users input
+            await newUser.save(); //save it to the database
+            //if status is success show response
+            res.status(200).send({ message: "User created successfully", success: true });
+            console.log(newUser);
+        }
+
     } catch (error) {
-        
-    } 
+        res.status(500).send({ message: "Error creating the user", success: false, error });
+    }
 })
 
 //login user
-router.post('/login',async(req,res)=>{
+router.post('/login', async (req, res) => {
     try {
-        
+
+
     } catch (error) {
-        
+
     }
 })
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const cors = require('cors');
 const app=express();
+
 require('dotenv').config()
+
+//to destructure the json format in reaable format
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(cors());
 
 //calling the mongodb connection
 const dbconfig = require("./config/dbconfig");
+
+//connecting user route
+const userRoute = require('./routes/userRoute');
+
+//creating api endpoints 
+app.use('/api/user',userRoute);
 
 //creating the port and executing it
 const port = process.env.PORT || 8000;
